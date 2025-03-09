ca821b44fe2426f207b6d5625e665627b05f2867
Ruwanthi Lakshika <125971277+RuwanthiLakshika@users.noreply.github.com>
2025-03-09 21:54:27 +0530

    changes in controller

---
+++ EmployeeAdminPortal/EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        //Guid - Globally Unique Identifier
+        public Guid Id { get; set; }
+        public  required string Name { get; set; }
+        public required string Email { get; set; }
+        //Declaring Phone as nullable
+        public string? Phone { get; set; }
+        public decimal Salary { get; set; }
+
+    }
+}
